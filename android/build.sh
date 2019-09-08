b4a836f7e154432f1c39932e3ca4f5236e84e3e3
Tom Powell <gitlab@tom.powell.io>
2019-09-08 02:53:03 +0000

    initial android build scripts

---
+++ android/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+export USE_CCACHE="1"
+export PYTHONDONTWRITEBYTECODE=true
+export BUILD_ENFORCE_SELINUX=1
+export BUILD_NO=
+unset BUILD_NUMBER
+#TODO(zif): convert this to a runtime check, grep "sse4_2.*popcnt" /proc/cpuinfo
+export CPU_SSE42=false
+# Following env is set from build
+# VERSION
+# DEVICE
+# TYPE
+# RELEASE_TYPE
+# EXP_PICK_CHANGES
+
+export BUILD_NUMBER=$( (date +%s%N ; echo $CI_PIPELINE_ID; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+
+cd /lineage/android
+rm -rf .repo/local_manifests/*
+if [ -f /lineage/setup.sh ]; then
+    source /lineage/setup.sh
+fi
+yes | repo init -u https://github.com/lineageos/android.git -b ${VERSION}
+echo "Resetting build tree"
+repo forall -vc "git reset --hard" > /tmp/android-reset.log 2>&1
+echo "Syncing"
+repo sync -j32 -d --force-sync > /tmp/android-sync.log 2>&1
+. build/envsetup.sh
+mka clobber
+set +e
+breakfast lineage_${DEVICE}-${TYPE}
+set -e
+if [ "$RELEASE_TYPE" '==' "experimental" ]; then
+  repopick $EXP_PICK_CHANGES
+fi
+mka otatools-package target-files-package dist
+
+#ssh jenkins@blob.lineageos.org mkdir -p  /home/jenkins/incoming/${DEVICE}/${CI_PIPELINE_ID}/
+#scp out/dist/*target_files*.zip jenkins@blob.lineageos.org:/home/jenkins/incoming/${DEVICE}/${CI_PIPELINE_ID}/
+#scp out/target/product/${DEVICE}/otatools.zip jenkins@blob.lineageos.org:/home/jenkins/incoming/${DEVICE}/${CI_PIPELINE_ID}/
---
+++ android/generator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import random
+import sys
+
+import yaml
+
+def main():
+    targets = sys.stdin.read()
+    pipeline = {"steps": []}
+    today = datetime.today()
+
+    for line in targets.split("\n"):
+        if not line or line.startswith("#"):
+            continue
+        device, release_type, version, cadence = line.split()
+        
+        # Only build monthly builds once a month, and only build weekly builds once a week.
+        if cadence == "M":
+            if random.Random(device).randint(1, 28) != today.day:
+                continue
+        elif cadence == "W":
+            if random.Random(device).randint(1, 7) != today.isoweekday():
+                continue
+
+        pipeline['steps'].append({
+            'label': '{} {} {} {}'.format(today.strftime("%Y%m%d"), device, version, cadence),
+            'trigger': 'android',
+            'build': {
+                'env': {
+                    'DEVICE': device,
+                    'RELEASE_TYPE': release_type,
+                    'VERSION': version,
+                },
+            },
+        })
+    print(yaml.dump(pipeline))
+
+if __name__ == '__main__':
+    main()
