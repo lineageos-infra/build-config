32ec23090f089d509a731dd3231abe12a78e6191
Tom Powell <tom@powell.io>
2020-02-25 20:04:30 -0800

    fixup repo usage
    build servers now have pyenv, init it here and use specific versions
    for repo and the build.
    
    tl;dr: google's decided repo will only work on 3.6+, but hasn't updated
    all of the android build tools to run on 3.6+...

--- android/build.sh
+++ android/build.sh
@@ -14,6 +14,12 @@ export CPU_SSE42=false
 # RELEASE_TYPE
 # EXP_PICK_CHANGES
 
+# setup pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+
 if [ -z "$BUILD_UUID" ]; then
   export BUILD_UUID=$(uuidgen)
 fi
@@ -26,6 +32,8 @@ export BUILD_NUMBER=$( (date +%s%N ; echo $BUILD_UUID; hostname) | openssl sha1 
 
 echo "--- Syncing"
 
+pyenv global 3.7.4
+
 cd /lineage/${VERSION}
 rm -rf .repo/local_manifests/*
 if [ -f /lineage/setup.sh ]; then
@@ -37,6 +45,8 @@ repo forall -vc "git reset --hard" > /tmp/android-reset.log 2>&1
 echo "Syncing"
 repo sync -j32 -d --force-sync > /tmp/android-sync.log 2>&1
 . build/envsetup.sh
+
+pyenv global 2.7.16
 
 echo "--- mka clobber"
 
