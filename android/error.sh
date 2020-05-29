73f076903033fbb74d46170a56308cb7aad22aab
Tom Powell <tom@powell.io>
2020-05-28 22:39:55 -0700

    fixup error handling

--- android/error.sh
+++ android/error.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -eu
-ssh jenkins@blob.lineageos.org mkdir -p /home/jenkins/incoming/${DEVICE}/${BUILD_UUID}/
+echo "--- Uploading logs on error"
+ssh jenkins@blob.lineageos.org mkdir -p /home/jenkins/incoming/failures/${DEVICE}/${BUILD_UUID}/
 scp /tmp/android-reset.log jenkins@blob.lineageos.org:/home/jenkins/incoming/failures/${DEVICE}/${BUILD_UUID}/
 scp /tmp/android-sync.log jenkins@blob.lineageos.org:/home/jenkins/incoming/failures/${DEVICE}/${BUILD_UUID}/
 scp /tmp/android-build.log jenkins@blob.lineageos.org:/home/jenkins/incoming/failures/${DEVICE}/${BUILD_UUID}/
