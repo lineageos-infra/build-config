54e7e0f7508873e81cc09e2ef6f20b900702362c
Tom Powell <tom@powell.io>
2021-02-15 17:04:09 -0800

    add experimental generator

---
+++ android/generator-android-experimental.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import random
+import sys
+import uuid
+
+import yaml
+
+def main():
+    targets = sys.stdin.read()
+    pipeline = {"steps": []}
+    today = datetime.today()
+
+    for line in targets.split("\n"):
+        if not line or line.startswith("#"):
+            continue
+        device, build_type, version = line.split()
+
+        pipeline['steps'].append({
+            'label': '{} experimental {}'.format(device, today.strftime("%Y%m%d")),
+            'trigger': 'android-experimental',
+            'build': {
+                'env': {
+                    'DEVICE': device,
+                    'RELEASE_TYPE': 'experimental',
+                    'TYPE': build_type,
+                    'VERSION': version,
+                    'BUILD_UUID': uuid.uuid4().hex,
+                },
+                'branch': version
+            },
+        })
+    print(yaml.dump(pipeline))
+
+if __name__ == '__main__':
+    main()
