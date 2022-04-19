8f3af4545e7d86a5b9b189fc26af0dedb82f7f42
Tom Powell <tom@powell.io>
2022-04-18 20:32:16 -0700

    fix ,

--- android-sync/generator.py
+++ android-sync/generator.py
@@ -21,7 +21,7 @@ def main():
                     'cd /lineage/{}'.format(version),
                     'yes | repo init -u https://github.com/lineageos/android.git -b {} || if [[ $? -eq 141 ]]; then true; else false; fi'.format(version),
                     'repo sync --prune --no-tags --force-remove-dirty --force-sync --verbose -j128',
-                ]
+                ],
                 'agents': ['queue={}'.format(host)],
             })
 
