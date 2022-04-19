218970d15b5ccd84fcef5c40a77dd37f64b7f5f1
Tom Powell <tom@powell.io>
2022-04-18 20:49:04 -0700

    i hate yaml

--- android-sync/generator.py
+++ android-sync/generator.py
@@ -17,11 +17,7 @@ def main():
 
             pipeline['steps'].append({
                 'label': 'sync {} {}'.format(host, version),
-                'commands': [
-                    'cd /lineage/{}'.format(version),
-                    'yes | repo init -u https://github.com/lineageos/android.git -b {} || if [[ $? -eq 141 ]]; then true; else false; fi'.format(version),
-                    'repo sync --prune --no-tags --force-remove-dirty --force-sync --verbose -j128',
-                ],
+                'command':  "cd /lineage/{} && (yes | repo init -u https://github.com/lineageos/android.git -b {} || if [[ $? -eq 141 ]]; then true; else false; fi) && repo sync --prune --no-tags --force-remove-dirty --force-sync --verbose -j128""".format(version, version),
                 'agents': ['queue={}'.format(host)],
             })
 
