55eade29b01bb159d8e1bee45f0854b838d270da
Tom Powell <tom@powell.io>
2022-06-14 23:19:15 -0700

    drop reset logs

--- android/error.sh
+++ android/error.sh
@@ -4,6 +4,5 @@ set -eu
 echo "--- Uploading logs on error"
 echo "failures/${DEVICE}/${BUILD_UUID}/"
 ssh jenkins@blob.lineageos.org mkdir -p /home/jenkins/incoming/failures/${DEVICE}/${BUILD_UUID}/
-scp /tmp/android-reset.log jenkins@blob.lineageos.org:/home/jenkins/incoming/failures/${DEVICE}/${BUILD_UUID}/
 scp /tmp/android-sync.log jenkins@blob.lineageos.org:/home/jenkins/incoming/failures/${DEVICE}/${BUILD_UUID}/
 scp /tmp/android-build.log jenkins@blob.lineageos.org:/home/jenkins/incoming/failures/${DEVICE}/${BUILD_UUID}/
