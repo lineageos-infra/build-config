d885a1db69328f5f26709a915f4444044970e1f6
Tom Powell <tom@powell.io>
2022-06-26 13:10:28 -0700

    fix github editor spacing

--- android-cleanup/generator.py
+++ android-cleanup/generator.py
@@ -2,22 +2,24 @@ import os
 import sys
 import yaml
 
+
 def main():
     pipeline = {"steps": []}
     host = os.environ.get("HOST", None)
     if not host:
         print("HOST environment variable required")
         sys.exit(-1)
-    
-    pipeline["steps"].append({
-        "label": f"cleanup for {host}",
-        "command": "uptime",
-        "agents": [
-            f"host={host}"
-        ]
-    })
-    
+
+    pipeline["steps"].append(
+        {
+            "label": f"cleanup for {host}",
+            "command": "uptime",
+            "agents": [f"host={host}"],
+        }
+    )
+
     print(yaml.dump(pipeline))
 
-    if __name__ == "__main__":
-        main()
+
+if __name__ == "__main__":
+    main()
