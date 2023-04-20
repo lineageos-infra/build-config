2118b8771f6fa5de2525f994dc820913e9665fd7
Tom Powell <tom@powell.io>
2023-04-19 17:44:15 -0700

    crowdin: use BUILDKITE_BRANCH instead of VERSION

--- crowdin/crowdin.sh
+++ crowdin/crowdin.sh
@@ -10,14 +10,12 @@ export BUILD_ENFORCE_SELINUX=1
 export BUILD_NO=
 unset BUILD_NUMBER
 
-if [ "$VERSION" == "lineage-18.1" ] || [ "$VERSION" == "lineage-19.1" ]; then
+if [ "$BUILDKITE_BRANCH" == "lineage-18.1" ] || [ "$BUILDKITE_BRANCH" == "lineage-19.1" ]; then
   export OVERRIDE_TARGET_FLATTEN_APEX=true
 fi
 
 #TODO(zif): convert this to a runtime check, grep "sse4_2.*popcnt" /proc/cpuinfo
 export CPU_SSE42=false
-# Following env is set from build
-# VERSION
 
 if [ -z "$REPO_VERSION" ]; then
   export REPO_VERSION=v2.28
@@ -28,15 +26,15 @@ SCRIPT_DIR=`pwd`
 
 echo "--- Syncing"
 
-mkdir -p /lineage/${VERSION}/.repo/local_manifests
-cd /lineage/${VERSION}
+mkdir -p /lineage/${BUILDKITE_BRANCH}/.repo/local_manifests
+cd /lineage/${BUILDKITE_BRANCH}
 rm -rf .repo/local_manifests/*
-cp lineage/crowdin/config/${VERSION}_extra_packages.xml .repo/local_manifests
+cp lineage/crowdin/config/${BUILDKITE_BRANCH}_extra_packages.xml .repo/local_manifests
 if [ -f /lineage/setup.sh ]; then
     source /lineage/setup.sh
 fi
 # catch SIGPIPE from yes
-yes | repo init -u https://github.com/lineageos/android.git -b ${VERSION} -g default,-darwin --repo-rev=${REPO_VERSION} || if [[ $? -eq 141 ]]; then true; else false; fi
+yes | repo init -u https://github.com/lineageos/android.git -b ${BUILDKITE_BRANCH} -g default,-darwin --repo-rev=${REPO_VERSION} || if [[ $? -eq 141 ]]; then true; else false; fi
 repo version
 
 echo "Syncing"
@@ -61,8 +59,8 @@ source /lineage/crowdin.sh
 pip3 install --user -r requirements.txt
 
 echo "--- download new translations"
-cd /lineage/${VERSION}
-./lineage/crowdin/crowdin_sync.py --username c3po --branch $VERSION --download -p "$SCRIPT_DIR/crowdin-cli.sh"
+cd /lineage/${BUILDKITE_BRANCH}
+./lineage/crowdin/crowdin_sync.py --username c3po --branch $BUILDKITE_BRANCH --download -p "$SCRIPT_DIR/crowdin-cli.sh"
 STATUS=$?
 
 if [[ $STATUS -eq 2 ]]; then
@@ -87,7 +85,7 @@ mka otatools-package target-files-package dist | tee /tmp/android-build.log || e
 # TODO(forkbomb): Abandon or -1 changes if stuff is broken.
 
 echo "--- Submitting translations"
-./lineage/crowdin/crowdin_sync.py --username c3po --branch $VERSION -s -o c3po
+./lineage/crowdin/crowdin_sync.py --username c3po --branch $BUILDKITE_BRANCH -s -o c3po
 echo "Successful"
 
 echo "--- cleanup"
