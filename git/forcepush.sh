15b85ebf743de2181f691eb76ed128c9da024a7d
Tom Powell <tom@powell.io>
2023-07-20 09:04:16 -0700

    add forcepush

---
+++ git/forcepush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+mkdir -p ${REPO}
+git clone https://github.com/${REPO} ${REPO} -b ${DEST_BRANCH}
+cd ${REPO}
+git remote add gerrit ssh://c3po@review.lineageos.org:29418/${REPO}
+git push gerrit HEAD:refs/heads/backups/$(date +%Y%m%d-%H%m)/${DEST_BRANCH})
+git remote add new ${SRC_REPO}
+git fetch new ${SRC_BRANCH}
+git checkout FETCH_HEAD
+git push --force gerrit HEADS:refs/heads/${DEST_BRANCH}
+
