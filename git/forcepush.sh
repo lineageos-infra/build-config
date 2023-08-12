406a1bb000f37016838de441fc561174e5951d66
Tom Powell <tom@powell.io>
2023-08-12 01:52:31 -0700

    Update forcepush.sh

--- git/forcepush.sh
+++ git/forcepush.sh
@@ -10,5 +10,5 @@ git push gerrit HEAD:refs/backups/heads/$(date +%Y%m%d-%H%m)/${DEST_BRANCH}
 git remote add new ${SRC_REPO}
 git fetch new ${SRC_BRANCH}
 git checkout FETCH_HEAD
-git push --force gerrit HEADS:refs/heads/${DEST_BRANCH}
+git push --force gerrit HEAD:refs/heads/${DEST_BRANCH}
 
