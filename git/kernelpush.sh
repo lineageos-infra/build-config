b856093c92a27eb01168b6e9dfee80d958c8d4c9
LuK1337 <priv.luk@gmail.com>
2026-02-11 23:49:33 +0100

    kernelpush: It's ok to fail push, maybe next one will succeed

--- git/kernelpush.sh
+++ git/kernelpush.sh
@@ -8,6 +8,7 @@ cd ${REPO}
 git remote add gerrit ssh://c3po@review.lineageos.org:29418/LineageOS/${REPO}
 for i in `seq 50000 -5000 0`; do
     git push -o skip-validation gerrit ${SRC_BRANCH}^0~${i}:refs/heads/${DEST_BRANCH} ||
-        git push --no-thin -o skip-validation gerrit ${SRC_BRANCH}^0~${i}:refs/heads/${DEST_BRANCH}
+        git push --no-thin -o skip-validation gerrit ${SRC_BRANCH}^0~${i}:refs/heads/${DEST_BRANCH} ||
+        true
     ssh -p 29418 c3po@review.lineageos.org replication start LineageOS/${REPO} --wait
 done
